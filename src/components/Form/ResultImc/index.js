786244e4bdd94738be815ec1b68a92464d423f04
Matheus <mthsferreirap@gmail.com>
2023-01-15 22:04:00 -0300

    Styling resultImc

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function ResultImc(props) {
     return (
-        <View>
-            <Text>{ props.messageResultImc }</Text>
-            <Text>{ props.resultImc }</Text>
+        <View styles={styles.resultImc}>
+            <Text styles={styles.information}> {props.messageResultImc}</Text>
+            <Text styles={styles.numberImc}>{props.resultImc}</Text>
         </View>
     );
 }
