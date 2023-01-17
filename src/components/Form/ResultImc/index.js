f28dc2e23626a052c603015ddd38a6c75c08bcf9
Matheus <mthsferreirap@gmail.com>
2023-01-16 23:44:41 -0300

    Add share button

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Share, TouchableOpacity } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props) {
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu imc hoje é:" + props.resultImc
+        })
+    }
+
     return (
         <View style={styles.resultImc}>
+            <View>
+                {props.resultImc != null ?
+                    <TouchableOpacity>
+                        <Text>Share</Text>
+                    </TouchableOpacity>
+                    :
+                    <View />
+                }
+            </View>
             <Text style={styles.information}> {props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
